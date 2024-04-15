9418487d2f04de38a3fce4d25f6aeefc027237f0
pandyama <pandyama2@gmail.com>
2024-04-14 19:39:38 -0600

    more comments removed

--- routes/root.js
+++ routes/root.js
@@ -1,24 +1,12 @@
 var express = require("express");
 var mainController = require("../controller/UserController");
-// var form_controller = require("../controller/formController");
 var router = express.Router();
 
 console.log("inside root.js");
+
 router.post("/users/signup", mainController.register);
 
-// router.post("/login", mainController.login); // Login Route
-
-// router.post("/logs", form_controller.logs);
-
-// router.get("/auth", verifyToken, function (req, res) {
-//   console.log("----------------------");
-//   console.log("Request Received at /auth");
-//   console.log("----------------------");
-//   return res.status(200).json({ message: "authenticated" });
-// });
-
 router.get("/fail", function (request, response, next) {
-  // console.log("🚀 ~ request:", request);
   console.log("fail");
   return response.status(200).json({ message: "failed" });
 });
